f7c5497649062ec32a81f284c585a2e563f2c784
Dileep Thiriveedhi <dileep2424@gmail.com>
2023-02-15 17:45:37 +0530

    looping multi items

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import "./components/ExpensiveItem/ExpensiveItem";
+
 import ExpensiveItem from "./components/ExpensiveItem/ExpensiveItem";
 
 function App() {
-  const expense = [
-    {
-      title: "food",
-      ammount: 10,
-      date: new Date(),
-      LocationOfExpenditure: "Secuendrabad",
-    },
-    {
-      title: "petrole",
-      ammount: 100,
-      date: new Date(),
-      LocationOfExpenditure: "KPHB",
-    },
-  ];
   return (
     <div>
       <h2>Expensive Tracker</h2>
-      <ExpensiveItem
-        title={expense[0].title}
-        ammount={expense[0].ammount}
-        date={expense[0].date}
-        LocationOfExpenditure={expense[0].LocationOfExpenditure}
-      />
-      <ExpensiveItem
-        title={expense[1].title}
-        ammount={expense[1].ammount}
-        date={expense[1].date}
-        LocationOfExpenditure={expense[1].LocationOfExpenditure}
-      />
+      <ExpensiveItem />
     </div>
   );
 }
--- src/components/ExpensiveItem/ExpensiveItem.js
+++ src/components/ExpensiveItem/ExpensiveItem.js
@@ -1,17 +1,33 @@
 import "./ExpensiveTracker.css";
 
 function ExpensiveItem(props) {
+  const expense = [
+    {
+      title: "food",
+      ammount: 10,
+      date: new Date(),
+      LocationOfExpenditure: "Secuendrabad",
+    },
+    {
+      title: "petrole",
+      ammount: 100,
+      date: new Date(),
+      LocationOfExpenditure: "KPHB",
+    },
+  ];
   return (
     <div>
-      <div className="expenses-list">
-        <h3>
-          {props.date.getFullYear()}-{props.date.getMonth()}-
-          {props.date.getDate()}
-        </h3>
-        <h3>{props.LocationOfExpenditure}</h3>
-        <h3>{props.title}</h3>
-        <div className="ammount-btn">RS:{props.ammount}</div>
-      </div>
+      {expense.map((each) => (
+        <div className="expenses-list">
+          <h3>
+            {each.date.getFullYear()}-{each.date.getMonth()}-
+            {each.date.getDate()}
+          </h3>
+          <h3>{each.LocationOfExpenditure}</h3>
+          <h3>{each.title}</h3>
+          <div className="ammount-btn">RS:{each.ammount}</div>
+        </div>
+      ))}
     </div>
   );
 }
