b5097392eeb5f5add1509edd0c8b036653925b0e
Dileep Thiriveedhi <dileep2424@gmail.com>
2023-02-22 11:24:34 +0530

    Using state to change by clicking button

--- src/App.js
+++ src/App.js
@@ -6,10 +6,35 @@ import Card from "./components/card/Card";
 import ExpensiveItem from "./components/ExpensiveItem/ExpensiveItem";
 
 const App = () => {
+  const expense = [
+    {
+      title: "food",
+      ammount: 10,
+      date: new Date(),
+      LocationOfExpenditure: "Secuendrabad",
+    },
+    {
+      title: "petrole",
+      ammount: 100,
+      date: new Date(),
+      LocationOfExpenditure: "KPHB",
+    },
+    {
+      title: "petrole",
+      ammount: 100,
+      date: new Date(),
+      LocationOfExpenditure: "KPHB",
+    },
+  ];
   return (
     <Card className="container">
       <h2>Expensive Tracker</h2>
-      <ExpensiveItem />
+      <ExpensiveItem
+        title={expense[0].title}
+        ammount={expense[0].ammount}
+        date={expense[0].date}
+        LocationOfExpenditure={expense[0].LocationOfExpenditure}
+      />
     </Card>
   );
 };
--- src/components/ExpensiveItem/ExpensiveItem.js
+++ src/components/ExpensiveItem/ExpensiveItem.js
@@ -1,39 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpensiveTracker.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../card/Card";
 
 const ExpensiveItem = (props) => {
-  const expense = [
-    {
-      title: "food",
-      ammount: 10,
-      date: new Date(),
-      LocationOfExpenditure: "Secuendrabad",
-    },
-    {
-      title: "petrole",
-      ammount: 100,
-      date: new Date(),
-      LocationOfExpenditure: "KPHB",
-    },
-    {
-      title: "petrole",
-      ammount: 100,
-      date: new Date(),
-      LocationOfExpenditure: "KPHB",
-    },
-  ];
+  const [title, setTitle] = useState(props.title);
+  const changeTitle = () => {
+    setTitle("Changed");
+    console.log(title);
+  };
+  const deleteItm = () => {};
   return (
     <Card className="expense-container">
-      {expense.map((each) => (
-        <Card className="expenses-list">
-          <ExpenseDate date={each.date} />
-          <h3>{each.LocationOfExpenditure}</h3>
-          <h3>{each.title}</h3>
-          <Card className="ammount-btn">RS:{each.ammount}</Card>
-        </Card>
-      ))}
+      <Card className="expenses-list">
+        <ExpenseDate date={props.date} />
+        <h3>{props.LocationOfExpenditure}</h3>
+        <h3>{title}</h3>
+        <Card className="ammount-btn">RS:{props.ammount}</Card>
+        <button onClick={changeTitle}>Edit</button>
+        <button onClick={deleteItm}>Delete</button>
+      </Card>
     </Card>
   );
 };
