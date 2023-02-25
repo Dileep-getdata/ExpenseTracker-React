f08b7597b8bff26a9fcc8c601812d264283ffe65
Dileep Thiriveedhi <dileep2424@gmail.com>
2023-02-25 09:36:51 +0530

    Usestate apply for ammount change to 100$

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
     },
     {
       title: "petrole",
-      ammount: 100,
+      ammount: 120,
       date: new Date(),
       LocationOfExpenditure: "KPHB",
     },
@@ -29,12 +29,26 @@ const App = () => {
   return (
     <Card className="container">
       <h2>Expensive Tracker</h2>
-      <ExpensiveItem
-        title={expense[0].title}
-        ammount={expense[0].ammount}
-        date={expense[0].date}
-        LocationOfExpenditure={expense[0].LocationOfExpenditure}
-      />
+      <Card className="expense-container">
+        <ExpensiveItem
+          title={expense[0].title}
+          ammount={expense[0].ammount}
+          date={expense[0].date}
+          LocationOfExpenditure={expense[0].LocationOfExpenditure}
+        />
+        <ExpensiveItem
+          title={expense[1].title}
+          ammount={expense[1].ammount}
+          date={expense[1].date}
+          LocationOfExpenditure={expense[1].LocationOfExpenditure}
+        />
+        <ExpensiveItem
+          title={expense[2].title}
+          ammount={expense[2].ammount}
+          date={expense[2].date}
+          LocationOfExpenditure={expense[2].LocationOfExpenditure}
+        />
+      </Card>
     </Card>
   );
 };
--- src/components/ExpensiveItem/ExpensiveItem.js
+++ src/components/ExpensiveItem/ExpensiveItem.js
@@ -4,22 +4,20 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../card/Card";
 
 const ExpensiveItem = (props) => {
-  const [title, setTitle] = useState(props.title);
+  const [ammount, setAmmount] = useState(props.ammount);
   const changeTitle = () => {
-    setTitle("Changed");
-    console.log(title);
+    setAmmount("100");
+    console.log(ammount);
   };
   const deleteItm = () => {};
   return (
-    <Card className="expense-container">
-      <Card className="expenses-list">
-        <ExpenseDate date={props.date} />
-        <h3>{props.LocationOfExpenditure}</h3>
-        <h3>{title}</h3>
-        <Card className="ammount-btn">RS:{props.ammount}</Card>
-        <button onClick={changeTitle}>Edit</button>
-        <button onClick={deleteItm}>Delete</button>
-      </Card>
+    <Card className="expenses-list">
+      <ExpenseDate date={props.date} />
+      <h3>{props.LocationOfExpenditure}</h3>
+      <h3>{props.title}</h3>
+      <Card className="ammount-btn">RS:{ammount}</Card>
+      <button onClick={changeTitle}>Amount(100$)</button>
+      <button onClick={deleteItm}>Delete</button>
     </Card>
   );
 };
